e0b7d1dac6fc92456887507e494ef4f3df85e60b
unknown <sh4dbr@gmail.com>
2020-07-10 14:36:37 -0300

    Implementing player class, controls and rendering

--- src/Game.java
+++ src/Game.java
@@ -1,9 +1,15 @@
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
 
 import javax.swing.JFrame;
 
-public class Game extends Canvas implements Runnable {
+public class Game extends Canvas implements Runnable, KeyListener {
 	
 
 	private static final long serialVersionUID = 1L;
@@ -11,8 +17,15 @@ public class Game extends Canvas implements Runnable {
 	public static int height = 120;
 	public static int scale = 3;
 	
+	public BufferedImage layer = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+	
+	public Player player;
+	
 	public Game() {
 		setPreferredSize(new Dimension(width*scale, height*scale));
+		this.addKeyListener(this);
+		
+		 player = new Player(100, height-10);
 	}
 	
 
@@ -27,12 +40,80 @@ public class Game extends Canvas implements Runnable {
 		frame.pack();
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
+		
+		new Thread(game).start();
 
+	}
+	
+	public void update() {
+		
+		player.update();
+		
+	}
+	
+	public void render() {
+		BufferStrategy bs = this.getBufferStrategy();
+		if(bs == null) {
+			this.createBufferStrategy(3);
+			return;
+		
+		}
+		Graphics g = layer.getGraphics();
+		g.setColor(Color.black);
+		g.fillRect(0,0, width, height);
+		player.render(g);
+		
+		g = bs.getDrawGraphics();
+		
+		g.drawImage(layer, 0, 0, width*scale, height*scale, null);
+		
+		bs.show();
+		
+		
 	}
 
 	@Override
 	public void run() {
+		while(true) {
+			update();
+			render();
+			try {
+				Thread.sleep(1000/60);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 		
+	}
+
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		
+	}
+
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			player.right = true;
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+			player.left = true;
+			
+		}
+	}
+
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			player.right = false;
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+			player.left = false;
+			
+		}
 	}
 
 }
